0d443d25aaa7776721082c8f00ad54a5f4973a63
DrFreudJr <drfreudj88@gmail.com>
2021-01-17 14:24:40 -0500

    Cleaned up code

--- everythingMethod.js
+++ everythingMethod.js
@@ -1,28 +1,18 @@
 /* Everything Exercise 
 Hand coded 'Every' method */
 'use strict';
-
- const array = [1,3,4,6,9];
-
 let cl = console.log;
-
 document.write("Javascript Active: " + Date())
 
-
-
-
-// let x = myEveryMethod(array, i => i < 10);
+const array = [1,3,4,6,9];
+let testFunction = x => x < 10;
 
 function everyMethod (array, myFunction) {
-    cl(array);
     for (let i in array) {
-        cl(array[i]);
         if (!myFunction(array[i]))
             return(false);
     }
     return(true);
 }
 
- let bool = everyMethod(array, x => x <10);
-
- cl(bool);
+console.log(everyMethod(array, testFunction));
