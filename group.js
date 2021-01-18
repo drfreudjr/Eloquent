728eeecfcf398128694b7cdc81e7340ce4a5eddf
DrFreudJr <drfreudj88@gmail.com>
2021-01-18 15:42:01 -0500

    Delete works code needs cleaning

--- group.js
+++ group.js
@@ -5,21 +5,38 @@ let cl = console.log;
 class Group {
     constructor(name){
 
-    this.array = [];
+    this.array = [1,2,3,4,5];
 
     this.add = function (itemToAdd) {
-        cl(`trying to add ${itemToAdd}`);
+        // cl(`trying to add ${itemToAdd}`);
             if (this.array.includes(itemToAdd) == true) 
                 console.log('Sorry item already in list');
-            else 
-                this.array.push(itemToAdd);         
+            else {
+                this.array.push(itemToAdd);
+                console.log(`${itemToAdd} has been added`);
+                console.log(`The list is now ${this.array}`);     
+            }
+    }
+    this.delete = function (itemToDelete) {
+        cl(`trying to delete ${itemToDelete}`);
+        if (this.array.includes(itemToDelete) == true) {
+            console.log(`Deleting ${itemToDelete}`);
+            let x = this.array.indexOf(itemToDelete);
+            console.log(`X is ${x}`);
+            this.array.splice(x,1);
+            console.log(`The list is now ${this.array}`);
+            }
+        else 
+            console.log('Sorry no such item');
         }
     }
 }
 
 let test = new Group();
+test.add(6);
+test.delete(1);
+test.delete(3);
 
-test.add(3);
+
 cl(test.array);
-test.add(3);
 
